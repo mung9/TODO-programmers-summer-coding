a612aeb1487e1b2b22f8f3ee945cd61d233fe0eb
mung9 <cutiemung2@naver.com>
2019-05-20 18:05:01 +0900

    footer 수정, 대쉬보드 화살표 위치 수정

--- client/src/components/commons/Footer.jsx
+++ client/src/components/commons/Footer.jsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <div className="subtitle"><h3>2019 Summer Coding</h3></div>
+      <div className="subtitle">
+        <h3>2019 Summer Coding</h3>
+        <p>Copyright 2019. mung9. All rights reserved.</p>
+      </div>
     </footer>
   );
 }
