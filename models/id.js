6de823f78840915a89c95b4a26776b049c66a6ef
mung9 <cutiemung2@naver.com>
2019-05-22 14:41:22 +0900

    불필요한 주석 제거

--- models/id.js
+++ models/id.js
@@ -4,13 +4,3 @@ Joi.objectId = require('joi-objectid')(Joi);
 module.exports = exports = function validateId(id){
   return Joi.validate({id}, {id: Joi.objectId()});
 }
-
-// var Joi = require('joi');
-// Joi.objectId = require('joi-objectid')(Joi);
- 
-// var schema = {
-//   id: Joi.objectId()
-// , name: Joi.string().max(100)
-// , date: Joi.date()
-// }
- 
